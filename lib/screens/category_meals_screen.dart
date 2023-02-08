fb324a9d8ac4d14d7325d55d671b60df323e909a
Bruno Codeman <brunobemfica@gmail.com>
2023-02-07 23:25:10 -0300

    Finished Section 7

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -20,7 +20,6 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
 
   @override
   void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
     if (!_loadedInitData) {
       final routeArgs =
           ModalRoute.of(context).settings.arguments as Map<String, String>;
